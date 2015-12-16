2ccc0bd31c57a386b1b7ce62eb3e6e2e4d18fb08
AutomatedTester <david.burns@theautomatedtester.co.uk>
2015-12-16 15:06:47 +0000

    Addressing comments from AMO-Reviewers

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-var self = require('sdk/self');
 var pageMod = require("sdk/page-mod");
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
 pageMod.PageMod({
   include: "https://twitter.com/",
   contentScript: "var MomentsButton = document.querySelector('.moments');" +
   "if(MomentsButton !== null){" +
-  	"MomentsButton.parentNode.removeChild(document.querySelector('.moments'));" +
+  	"MomentsButton.parentNode.removeChild(MomentsButton);" +
   "}"
 });
