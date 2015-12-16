ed2606a83927186b40ef58c6ff57d42f0617303c
AutomatedTester <david.burns@theautomatedtester.co.uk>
2015-12-16 15:13:43 +0000

    Correct paths that this will work on
    There is an issue that it would not work against https://twitter.com/anyurl.
    This adds a wildcard after the / to handle all.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var pageMod = require("sdk/page-mod");
 
 pageMod.PageMod({
-  include: "https://twitter.com/",
+  include: "https://twitter.com/*",
   contentScript: "var MomentsButton = document.querySelector('.moments');" +
   "if(MomentsButton !== null){" +
   	"MomentsButton.parentNode.removeChild(MomentsButton);" +
